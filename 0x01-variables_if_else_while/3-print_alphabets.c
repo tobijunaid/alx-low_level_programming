d6a9c2c9c3c4dfbf7090e1be4470528930750d7e
Tobi Junaid <tobijunaid2@gmail.com>
2022-09-12 02:16:00 -0700

    try

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+
+{
+	    int ch;
+
+	        for (ch= 'A'; ch<= 'Z'; ch++)
+			        putchar(ch);
+		    putchar (' ') ; for (ch= 'a'; ch<= 'z'; ch++)
+			            putchar(ch);
+		        putchar (' ') ;
+}
