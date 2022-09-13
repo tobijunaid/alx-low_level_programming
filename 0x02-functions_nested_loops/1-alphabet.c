55a43ecbadc6b76afb85b35edc2216e8556a36a4
Tobi Junaid <tobijunaid2@gmail.com>
2022-09-13 10:18:17 -0700

    try

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+	    print_alphabet(void);
+}
+
+void print_alphabet(void)
+{
+	    char ch = 'a';
+
+	        while(ch <= 'z') {
+			        printf("%c", ch);
+				        ch++;
+					    }
+		    printf("\n");
+
+		        return void;
+}
