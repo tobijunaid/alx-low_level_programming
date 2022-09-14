c7019ac9fee6551ab9d43c7b76e53f3baad1ed21
Tobi Junaid <tobijunaid2@gmail.com>
2022-09-14 13:05:02 -0700

    try

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+void print_alphabet_x10 (void)
+{
+	    char c;
+	        int i = 0;
+
+		    while (i <= 9) {
+			            for (c = 'a'; c <= 'z'; c++) {
+					                putchar(c);
+							        }
+
+				            putchar('\n');
+
+					            i++;
+						        }
+}
