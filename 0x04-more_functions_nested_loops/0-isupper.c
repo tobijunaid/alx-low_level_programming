efdb4c944d7b6c968b4318802bd4d30dc1f106a0
Tobi Junaid <tobijunaid2@gmail.com>
2022-09-15 12:21:23 -0700

    try

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,19 @@
+#include "main.h"
 #include <stdio.h>
 #include <ctype.h>
-int main()
+
+/**
+ *  * main - check the code.
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
 {
 	    char c;
 
-	        c = 'C';
-		    printf("Return value when uppercase character %c is passed to isupper(): %d", c, isupper(c));
-
-		        c = '+';
-			    printf("\nReturn value when another character %c is passed to is isupper(): %d", c, isupper(c));
-
-			       return 0;
+	        c = 'A';
+		    printf("%c: %d\n", c, isupper(c));
+		        c = 'a';
+			    printf("%c: %d\n", c, isupper(c));
+			        return (0);
 }
